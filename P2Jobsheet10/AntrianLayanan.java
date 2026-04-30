d8321ee11ac8fdc2ba965aed8db5f8153e4caf0b
adisnayottama <adisnayottama1@gmail.com>
2026-04-29 23:25:16 -1200

    Prk2 Class2 Modif

--- P2Jobsheet10/AntrianLayanan.java
+++ P2Jobsheet10/AntrianLayanan.java
@@ -79,4 +79,14 @@ public class AntrianLayanan {
 
         System.out.println("Antrian berhasil dikosongkan");
     }
+
+    public void lihatAkhir() {
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            System.out.println("Mahasiswa paling belakang");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
 }
